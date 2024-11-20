ab3993829504abe712e266dd61a580a70ac03ee3
mustafa altuntaş <altun_tas@outlook.com>
2024-11-20 20:29:14 +0300

    .

---
+++ AuthServer.Core/Services/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using AuthServer.Core.Configurations;
+using AuthServer.Core.Dtos;
+using AuthServer.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreteToken(UserApp userApp);
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
