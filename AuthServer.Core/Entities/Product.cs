0035c2174125a577cd6b4274eacf55e57d9498a2
mustafa altuntaş <altun_tas@outlook.com>
2024-11-22 15:29:46 +0300

    .

--- AuthServer.Core/Entities/Product.cs
+++ AuthServer.Core/Entities/Product.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace AuthServer.Core.Entities
 {
-    public class Product
+    public class Product : IBaseEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public Decimal Price { get; set; }
         public int Stock { get; set; }
         public string UserId { get; set; }
-
+        public DateTime CreatedAt { get; set; }
     }
 }
