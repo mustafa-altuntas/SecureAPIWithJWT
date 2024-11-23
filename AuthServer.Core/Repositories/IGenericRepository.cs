59be1a6389801db32331a7dc70a3c92fed06df73
mustafa altuntaş <altun_tas@outlook.com>
2024-11-23 14:10:06 +0300

    .

--- AuthServer.Core/Repositories/IGenericRepository.cs
+++ AuthServer.Core/Repositories/IGenericRepository.cs
@@ -10,7 +10,7 @@ namespace AuthServer.Core.Repositories
     public interface IGenericRepository<TEntity> where TEntity : class
     {
         Task<TEntity> GetByIdAsync(int id);
-        Task<IQueryable<TEntity>> GetAllAsync();
+        IQueryable<TEntity> GetAll();
         IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
         Task AddAsync(TEntity entity);
         void Remove(TEntity entity);
