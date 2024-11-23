931ae601ad40252d9c1361df1f53f5bf23971ac3
mustafa altuntaş <altun_tas@outlook.com>
2024-11-23 14:16:11 +0300

    UnitOfWork oluşturuldu

---
+++ AuthServer.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using AuthServer.Core.UnitOfWork;
+using AuthServer.Data.DataContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+
+        public UnitOfWork(SecureApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
