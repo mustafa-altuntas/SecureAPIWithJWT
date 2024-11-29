e62f814655593105ac2fe71dbc3873257dc3dee5
mustafa altuntaş <altun_tas@outlook.com>
2024-11-29 17:22:51 +0300

    feat: Add AuthController for authentication-related operations

---
+++ AuthServer.API/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : CustomBaseController
+    {
+
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        public async  Task<IActionResult> CreateToken(LoginDto loginDto)
+        {
+            var result = await _authenticationService.CreateTokenAsync(loginDto);
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            var result = await _authenticationService.CreateTokenByClient(clientLoginDto);
+
+            return ActionResultInstance(result);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> RevokeRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            var result = await _authenticationService.RevokeRefreshToken(refreshTokenDto.Token);
+
+            return ActionResultInstance(result);
+        }
+          
+        [HttpPost]
+        public async Task<IActionResult> CreateTokenByRefreshtoken(RefreshTokenDto refreshTokenDto)
+        {
+            var result = await _authenticationService.CreateTokenByRefreshToken(refreshTokenDto.Token);
+
+            return ActionResultInstance(result);
+        }
+
+    }
+}
