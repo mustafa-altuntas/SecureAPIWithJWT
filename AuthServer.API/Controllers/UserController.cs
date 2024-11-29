9b971c51f0a75710220691a08cd98e0d9f932de8
mustafa altuntaş <altun_tas@outlook.com>
2024-11-29 17:32:37 +0300

    feat: Add UserController for user management operations

---
+++ AuthServer.API/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : CustomBaseController
+    {
+
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserDto createUserDto)
+        {
+            var result = await _userService.CreateUserAsync(createUserDto);
+            return ActionResultInstance(result);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            return ActionResultInstance(await _userService.GetUserByNameAsync(HttpContext.User.Identity.Name));
+        }
+
+    }
+}
