5eb43491d5d8a21cc7b72dcad4fd4e966007f97f
mustafa altuntaş <altun_tas@outlook.com>
2024-11-30 17:16:44 +0300

    feat: Add CustomTokenAuthExtension for JWT authentication setup

---
+++ SharedLib/SharedLibrary/Extensions/CustomTokenAuthExtension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using SharedLibrary.Configurations;
+using SharedLibrary.Services;
+
+
+namespace SharedLibrary.Extensions
+{
+    public static class CustomTokenAuthExtension
+    {
+        public static void AddCudtomTokenAuth(this IServiceCollection services, CustomTokenOptions tokenOptions)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opt =>
+            {
+                opt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidIssuer = tokenOptions.Issuer,
+                    ValidAudience = tokenOptions.Audience[0],
+                    IssuerSigningKey = SecurityKeyService.GetSymmetricSecurityKey(tokenOptions.SecurityKey),
+
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+        }
+    }
+}
