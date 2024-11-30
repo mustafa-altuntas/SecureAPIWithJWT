a05b5fc7dc1b86294ea3f467570b7d1642e96ad7
mustafa altuntaş <altun_tas@outlook.com>
2024-11-30 17:44:35 +0300

    feat: Add JWT authentication configuration to MiniApp3 Program.cs

--- OtherAPIs/MiniApp3.Api/Program.cs
+++ OtherAPIs/MiniApp3.Api/Program.cs
@@ -1,6 +1,16 @@
+using SharedLibrary.Configurations;
+using SharedLibrary.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.Configure<CustomTokenOptions>(builder.Configuration.GetSection("TokenOptions"));
+var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<CustomTokenOptions>();
+
+builder.Services.AddCudtomTokenAuth(tokenOptions);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,6 +28,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
